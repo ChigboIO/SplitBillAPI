c39dc9d00c007fcc730359b2dfa69d261811cba3
Emmanuel Chigbo <emmanuel.chigbo@andela.com>
2017-09-05 14:02:40 +0100

    Implement creating bill and splitting after bill is created

--- app/controllers/api/bills_controller.rb
+++ app/controllers/api/bills_controller.rb
@@ -1,8 +1,13 @@
 class Api::BillsController < Api::BaseController
   def index
+    render json: current_user.bills
   end
 
   def create
+    @bill = current_user.bills.build(bill_params)
+    render json: @bill, status: 201 and return if @bill.save
+
+    render json: @bill.errors.full_messages, status: 400
   end
 
   def show
@@ -12,5 +17,11 @@ class Api::BillsController < Api::BaseController
   end
 
   def delete
+  end
+
+  private
+
+  def bill_params
+    params.permit(:amount, :title, :description, :date, splitters: [])
   end
 end
--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,4 +1,6 @@
 class Bill < ApplicationRecord
+  after_create :split_bill
+
   belongs_to :creator, class_name: 'User'
   has_many :splits
 
@@ -6,5 +8,14 @@ class Bill < ApplicationRecord
 
   def splitters
     splits.map(&:user)
+  end
+
+  def split_bill
+    # This splitting can be abstracted to a service. Or even a background job
+    splitters_count = splitters.count
+    splitters.each do |splitter|
+      user = User.find_by(username: splitter)
+      splits.create(payer: user, amount: (amount / splitters_count).ceil)
+    end
   end
 end
