7751aebb2d8b18fd626f807af0241c08130ff908
Emmanuel Chigbo <emmanuel.chigbo@andela.com>
2017-09-05 08:24:33 +0100

    Add jwt_provider class

---
+++ app/classes/jwt_provider.rb
@@ -0,0 +1,15 @@
+class JwtProvider
+  def self.encode(payload, expiration = 1.hour.from_now)
+    payload = payload.dup
+    payload[:exp] = expiration.to_i
+    token = JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    Token.create(value: token)
+    token
+  end
+
+  def self.decode(token)
+    JWT.decode(token, Rails.application.secrets.secret_key_base, true).first
+  rescue
+    nil
+  end
+end
