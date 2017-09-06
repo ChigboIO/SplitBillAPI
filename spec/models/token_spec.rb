658327edeea607f6b1ff6e9c83154f920c9ca3ad
Emmanuel Chigbo <emmanuel.chigbo@andela.com>
2017-09-06 11:29:24 +0100

    Add model tests for Token

---
+++ spec/models/token_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { build(:token) }
+
+  describe 'Model from Factory' do
+    it { is_expected.to be_valid }
+
+    context 'when any of the required fields is nil' do
+      it { expect(build(:token, value: nil)).not_to be_valid }
+    end
+  end
+
+  describe 'Validators' do
+    it { is_expected.to validate_presence_of(:value) }
+  end
+
+  describe 'Active model associations' do
+    it { is_expected.to belong_to(:user) }
+  end
+end
