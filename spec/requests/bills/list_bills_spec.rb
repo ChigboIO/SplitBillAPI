6092bc43d9126d58315d6050547a473366362ed2
Emmanuel Chigbo <emmanuel.chigbo@andela.com>
2017-09-11 07:50:34 +0100

    Add request tests for listing bills

---
+++ spec/requests/bills/list_bills_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'List bill request', type: :request do
+  context 'with valid auth token' do
+    let(:user) { create(:user) }
+    let(:token) { JwtProvider.encode(user_id: user.id) }
+    before(:each) { user.tokens.create(value: token) }
+
+    it 'has 200 response code' do
+      get api_bills_path, params: {}, headers: { HTTP_AUTHORIZATION: token }
+
+      expect(response).to have_http_status 200
+    end
+
+    context 'when user created 2 bills' do
+      before(:each) { create_list(:bill, 2, creator: user) }
+
+      it 'returns 2 bills in the json response' do
+        get api_bills_path, params: {}, headers: { HTTP_AUTHORIZATION: token }
+
+        expect(parsed_body.count).to eql 2
+      end
+    end
+
+    context 'when you creats 2 bills and is tagged in 2' do
+      before(:each) do
+        create_list(:bill, 2, creator: user)
+        create_list(:bill, 2, splitters: [user.username, 'another_abitrary_user'])
+      end
+
+      it 'returns 4 bills in the json response' do
+        get api_bills_path, params: {}, headers: { HTTP_AUTHORIZATION: token }
+
+        expect(parsed_body.count).to eql 4
+      end
+    end
+  end
+end
